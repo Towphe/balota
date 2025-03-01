c431bdcaa8ebb7adce523410734298858a8be15e
Towphe <castillochristopherjames@gmail.com>
2025-03-01 19:06:58 +0800

    configured main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,18 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Button } from "@/components/ui/button";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { AppSidebar } from "@/components/AppSidebar";
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +37,48 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} `}
+        className={`${geistSans.variable} ${geistMono.variable}`}
       >
-        {children}
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarInset>
+          <div className="grid grid-rows-[auto_1fr_auto] min-h-screen">
+            <header className="min-w-[calc(100vw-16rem)] max-w-screen px-2 py-4 flex justify-between items-center">
+              <SidebarTrigger />
+              <h1 className="!text-xl">Balota</h1>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button className="!bg-yellow-500 !rounded-md">
+                    Donate
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="w-11/12 md:w-full">
+                  <DialogHeader>
+                    <DialogTitle>Buy me a coffee!</DialogTitle>
+                    <div className="w-full flex flex-col md:flex-row items-center space-between">
+                      <img src="/tope-gcash.png" className="w-[12rem]" alt="Tope's GCash QR Code" />
+                      <div>
+                        <h3 className="font-semibold">GCash Account Info</h3>
+                        <p>Name: CH********R JA**S C.</p>
+                        <p>Mobile No.: 096*****469</p>
+                        <p>User ID: ***********3GV0T4</p>
+                      </div>
+                    </div>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
+            </header>
+            <main>
+              {/* main */}
+              {children}
+            </main>
+            <footer className="px-4 pb-3">
+              {/* footer */}
+              <p className="text-xs opacity-60">Balota.io, by Tope, All rights reserved</p>
+            </footer>
+          </div>
+          </SidebarInset>
+        </SidebarProvider>
       </body>
     </html>
   );
---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,99 @@
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+  } from "@/components/ui/sidebar"
+import { Collapsible, CollapsibleTrigger, CollapsibleContent } from "./ui/collapsible"
+import Link from "next/link"
+  
+  export function AppSidebar() {
+
+    return (
+      <Sidebar>
+        <SidebarContent>
+            <SidebarGroup>
+                <SidebarGroupLabel className="!mt-8 !py-2">Balota</SidebarGroupLabel>
+                <SidebarGroupContent>
+                    <SidebarMenu>
+                        <SidebarMenuItem className="!py-2">
+                            <Link href="/">
+                                <SidebarMenuButton className="!flex items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                        <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+                                        <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+                                    </svg>
+                                    <span>Home</span>
+                                </SidebarMenuButton>
+                            </Link>
+                        </SidebarMenuItem>
+                        <SidebarMenuItem className="!py-2">
+                            <Link href="balota">
+                                <SidebarMenuButton className="!flex items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                        <path d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625Z" />
+                                        <path d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
+                                    </svg>
+                                    <span>Balota</span>
+                                </SidebarMenuButton>
+                            </Link>
+                        </SidebarMenuItem>
+                        <SidebarMenuItem className="!py-2">
+                            <Link href="/statistics">
+                                <SidebarMenuButton className="!flex items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                        <path d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75ZM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 0 1-1.875-1.875V8.625ZM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 0 1 3 19.875v-6.75Z" />
+                                    </svg>
+                                    <span>Statistics</span>
+                                </SidebarMenuButton>
+                            </Link>
+                        </SidebarMenuItem>
+                        <Collapsible defaultOpen={false} className="group/collapsible">
+                            <SidebarMenuItem>
+                                <CollapsibleTrigger  className="">
+                                    <div className="!w-[16rem] md:!w-[15rem] !flex items-center justify-between p-2">
+                                        <div className="!flex items-center gap-1">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+                                                <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
+                                            </svg>
+                                            <span>Candidates</span>
+                                        </div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+                                            <path fillRule="evenodd" d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z" clipRule="evenodd" />
+                                        </svg>
+                                        </div>
+                                </CollapsibleTrigger>
+                                <CollapsibleContent>
+                                    <SidebarGroup>
+                                        <SidebarGroupContent>
+                                        <Link href="/senators">
+                                            <SidebarMenuButton className="!flex items-center">Senators</SidebarMenuButton>
+                                        </Link>
+                                        <Link href="/partylists">
+                                            <SidebarMenuButton className="!flex items-center">Partylists</SidebarMenuButton>
+                                        </Link>
+                                        <Link href="/local-candidates">
+                                            <SidebarMenuButton className="!flex items-center">Local Candidates</SidebarMenuButton>
+                                        </Link>
+                                        </SidebarGroupContent>
+                                    </SidebarGroup>
+                                </CollapsibleContent>
+                            </SidebarMenuItem>
+                        </Collapsible>
+                        <SidebarMenuItem className="!py-2">
+                            
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarGroupContent>
+            </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+    )
+  }
+  
