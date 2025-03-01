74bdbc23a07f80078f9651f1355826fc10d5c88d
Towphe <castillochristopherjames@gmail.com>
2025-03-01 19:09:02 +0800

    setup landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,42 @@
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen">
-      <header>
-        {/* header */}
-      </header>
-      
-    </main>
+    <div className="h-full flex flex-col items-center justify-center">
+      <h1 className="text-5xl">Balota</h1>
+      <p className="opacity-50 mb-2">Helping voters, one ballot at a time</p>
+      <div className="flex flex-col items-center gap-2">
+        <Button className="w-[13rem] rounded-xl bg-[#1357BE] !py-1">Generate Ballot</Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="w-[13rem] rounded-xl bg-white text-black !py-1 shadow-md focus:bg-white">View Candidates</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-[13rem]">
+            <DropdownMenuLabel>Position</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href="/senators" className="!w-full">
+                Senators
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/partylists" className="!w-full">
+                Partylist
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/local-candidates" className="!w-full">
+                Local Positions
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        
+      </div>
+    </div>
   );
 }
