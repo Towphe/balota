26bc9d0a3503499a344dd065d0ae368d4f01bf73
Towphe <castillochristopherjames@gmail.com>
2025-03-10 08:11:45 +0800

    added adsense snippet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8793098467072920"
+     crossOrigin="anonymous"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable}`}
       >
